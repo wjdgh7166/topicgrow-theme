af20b33802adfa33da2293ac8c2389843a40da4f
Songjungho <songjungho@Songjunghoui-MacBookAir.local>
2026-08-19 13:16:35 +0900

    Remove duplicate featured image render on single post
    Featured image was auto-rendered above the content, but posts also
    set the featured image to the first content image, causing it to
    show twice.

--- single.php
+++ single.php
@@ -11,9 +11,6 @@
         <?php echo get_avatar(get_the_author_meta('ID'), 20, '', '', ['class' => 'avatar']); ?>
         <span><?php the_author(); ?> · <?php echo get_the_date('Y.m.d'); ?></span>
       </div>
-      <?php if (has_post_thumbnail()) : ?>
-        <div class="single-thumb"><?php the_post_thumbnail('full'); ?></div>
-      <?php endif; ?>
       <div class="entry-content">
         <?php the_content(); ?>
       </div>
